5b76d901b1f6fc0c0eec7eab237b6df3b4dd1d6c
Sam B. <mslserp.website1.ph@gmail.com>
2025-11-19 21:35:49 +0800

    Fix vite build directory

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,18 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/gtac/',
+    base: '/gtac/', // keep this since your site is in domain.com/gtac
     plugins: [
         laravel({
             input: ['resources/js/app.jsx'],
             publicDirectory: 'public',
-            buildDirectory: 'build',
+            buildDirectory: 'build', // Vite will output to public/build automatically
             refresh: true,
         }),
         react(),
     ],
     build: {
-        outDir: 'build',
+        outDir: 'public/build',   // <-- MUST BE inside public
         emptyOutDir: true,
     }
 });
