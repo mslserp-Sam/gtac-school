d360500126fdaa1f2e035507afcf5a7a432fd881
Sam B. <mslserp.website1.ph@gmail.com>
2025-12-10 08:34:07 +0800

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,18 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/gtac/', // keep this since your site is in domain.com/gtac
+    base: '/gtac/build/',
     plugins: [
         laravel({
             input: ['resources/js/app.jsx'],
             publicDirectory: 'public',
-            buildDirectory: 'build', // Vite will output to public/build automatically
+            buildDirectory: 'build',
             refresh: true,
         }),
         react(),
     ],
     build: {
-        outDir: 'public/build',   // <-- MUST BE inside public
+        outDir: 'public/build',
         emptyOutDir: true,
     }
 });
